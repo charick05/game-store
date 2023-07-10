ac0b32ca23e698b80cc90befa354b296502e09cb
Magomed <charick05@gamil.com>
2023-07-10 16:32:56 +0300

    fix bags

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -8,7 +8,6 @@ import './search.scss';
 const Search: React.FC = () => {
 
    const [value, setValue] = useState('');
-   const [inputVisible, setInputVisible] = useState<boolean>(window.innerWidth >= 665);
    const [searchTimer, setSearchTimer] = useState<NodeJS.Timeout | null>(null);
    const inputRef = useRef<HTMLInputElement>(null);
    const dispatch = useAppDispatch();
@@ -43,40 +42,22 @@ const Search: React.FC = () => {
       inputRef.current?.focus();
    }
 
-
-   useEffect(() => {
-    const handleResize = () => {
-      setInputVisible(window.innerWidth >= 665)
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    }
-   }, []);
-
-
-   const toggleInputVisible = () => {
-    setInputVisible(!inputVisible);
-   }
-
   return (
    <div className="search">
-      {inputVisible && <input
+      <input
          ref={inputRef}
          type="text" 
          value={value} 
          onChange={e => filterGames(e.target.value)} 
-      />}
-      {value && inputVisible && <MdClear 
+      />
+      {value && <MdClear 
          className='search_clear'
          onClick={searchClear}
          size={22}
       />}
-      <BiSearchAlt 
-        onClick={toggleInputVisible}
-        size={35} />
+      {!value && <BiSearchAlt 
+        className='search_icon'
+      />}
   </div>
   )
 }
